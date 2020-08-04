6ecb6acb8e34bdc264dc22e22ac7becb6e9be69e
Lestella <rehemae.v@gmail.com>
2020-08-04 17:29:56 +0300

    Ошибка_2 "Повышаем интеллектуальность функций login и logout"

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,8 @@ import time
 
 
 def test_add_group(app):
-    app.group.create(Group(name="Group 1", header="egwtg", footer="wrtrth"), word="pohui cho")
+    app.group.create(Group(name="Group 1", header="egwtg", footer="wrtrth"), word="hey")
     app.session.logout()
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""), word="pohui cho")
+    app.group.create(Group(name="", header="", footer=""), word="hey")
