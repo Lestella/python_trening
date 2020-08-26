32c795226ef0e26f2ae7dbc4f0c647d5694e13c5
Lestella <rehemae.v@gmail.com>
2020-08-26 15:06:30 +0300

    Type Error ощибка для Edit group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,19 @@
 from model.group import Group
+from random import randrange
 
 
 def test_edit_first_group_name(app):
     if app.group.count_groups() == 0:
         app.group.create(Group(name="Name For Edit"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    print(index)
     group = Group(name="New group")
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(group, index)
     assert len(old_groups) == app.group.count_groups()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
