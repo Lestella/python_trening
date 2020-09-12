95727d35b8104416cebc576b1cbe17cadbd99af4
Lestella <rehemae.v@gmail.com>
2020-09-12 17:25:05 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -12,14 +12,3 @@ def test_delete_some_group(app):
     new_groups = app.group.get_group_list()
     old_groups[index:index+1] = []
     assert old_groups == new_groups
-
-#
-# def test_del_first_group(app):
-#     if app.group.count_groups() == 0:
-#         app.group.create(Group(name="Name For Delete"))
-#     old_groups = app.group.get_group_list()
-#     app.group.delete_first_group()
-#     assert len(old_groups) - 1 == app.group.count_groups()
-#     new_groups = app.group.get_group_list()
-#     old_groups[0:1] = []
-#     assert old_groups == new_groups
